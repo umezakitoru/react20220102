fdbddcd82c8cab7fdfd7588d10681d2fc2e8d9f3
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-01-06 07:18:42 +0900

    111

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1ページです。</h1>
+      <Link to="/page1/detalA">DetailA</Link>
+      <br />
+      <Link to="/page1/detalB">DetailA</Link>
     </div>
   );
 };
